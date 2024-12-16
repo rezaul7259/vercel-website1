ae62ff2d2c088d38da816d936d7fd49e805f514e
rezaul7259 <arrajaul409@gmail.com>
2024-12-16 16:14:19 +0600

    Create Next.js

---
+++ Next.js
@@ -0,0 +1,17 @@
+/ (Root)
+|-- package.json
+|-- /pages
+|   |-- index.js
+|   |-- /api
+|       |-- submit.js
+|-- /components
+|   |-- Counter.js
+|   |-- Form.js
+|   |-- Footer.js
+|-- /public
+|   |-- image1.jpg
+|   |-- image2.jpg
+|   |-- image3.jpg
+|   |-- gif1.gif
+|   |-- gif2.gif
+|   |-- gif3.gif
